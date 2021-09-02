fe2043eb61fe6eb596da98c1fbdfeff5fb94d4ae
huatree <wang.yaohua@foxmail.com>
2021-09-02 23:42:06 +0800

    ✨ feat(书架): 实现页面、业务功能、接口联调

--- src/api/index.js
+++ src/api/index.js
@@ -2,18 +2,57 @@
  * @fileoverview api
  */
 
- import axios from 'axios'
+import axios from 'axios'
+import { setLocalForage } from '@/utils/localForage'
+import { getCategoryName } from '@/utils/book'
 
- export function home2() {
-   return axios({
-     method: 'get',
-     url: `${process.env.VUE_APP_BOOK_URL}/book/home2`
-   })
- }
+export function home2() {
+  return axios({
+    method: 'get',
+    url: `${process.env.VUE_APP_BOOK_URL}/book/home2`
+  })
+}
 
- export function home() {
-   return axios({
-     method: 'get',
-     url: `${process.env.VUE_APP_BASE_URL}/book/home`
-   })
- }
+export function home() {
+  return axios({
+    method: 'get',
+    url: `${process.env.VUE_APP_BASE_URL}/book/home`
+  })
+}
+
+export function shelf() {
+  return axios({
+    method: 'get',
+    url: `${process.env.VUE_APP_BASE_URL}/book/shelf`
+  })
+}
+
+export function download(item, onSuccess, onFailed, onError, onProgress) {
+  axios
+    .create({
+      baseURL: process.env.VUE_APP_EPUB_URL,
+      method: 'get',
+      responseType: 'blob',
+      timeout: 180 * 1000,
+      onDownloadProgress: progressEvent => {
+        if (onProgress) onProgress(progressEvent)
+      }
+    })
+    .get(`${getCategoryName(item.category)}/${item.fileName}.epub`)
+    .then(res => {
+      const blob = new Blob([res.data])
+      setLocalForage(
+        item.fileName,
+        blob,
+        () => {
+          if (onSuccess) onSuccess(item)
+        },
+        err => {
+          if (onFailed) onFailed(err)
+        }
+      )
+    })
+    .catch(err => {
+      if (onError) onError(err)
+    })
+}
